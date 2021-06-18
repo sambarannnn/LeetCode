c95054f18fa288e1bd45f0a9d240920022adc3c7
Sambaran Sengupta <63100608+sambarannnn@users.noreply.github.com>
2021-06-18 22:03:39 +0530

    Update Solution.java

--- Medium/328. Odd Even Linked List/Solution.java
+++ Medium/328. Odd Even Linked List/Solution.java
@@ -6,8 +6,7 @@
  *     ListNode() {}
  *     ListNode(int val) { this.val = val; }
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+ * }*/
 class Solution {
     public ListNode oddEvenList(ListNode head) {
         if(head == null) 
