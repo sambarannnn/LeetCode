79a2d4d8e8d2d32a94ec3a98d6a74e33d73e4208
Sambaran Sengupta <63100608+sambarannnn@users.noreply.github.com>
2021-06-21 14:18:35 +0530

    Update Solution.java

--- Easy/66. Plus One/Solution.java
+++ Easy/66. Plus One/Solution.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        
+        /*
         int n = digits.length;
         for(int i = n - 1; i >= 0; i--) {
             
@@ -15,5 +15,27 @@ class Solution {
         newNumber[0] = 1;
         
         return newNumber;
+        */
+        //CAN BE USED TO ADD ANY GIVEN NUMBER
+        int n = digits.length;
+        int carry = 0;
+        int tobeadded = 1;//from question
+        int sum;
+        for(int i = n - 1; i >= 0; i--) {
+            if(i == n - 1)
+                sum = digits[i] + tobeadded;
+            else
+                sum = digits[i] + carry;
+            digits[i] = (sum) % 10 ;
+            carry = sum / 10;
+            if(carry == 0)
+                return digits;
+        }
+        //in case entire input was 9999... 
+        int[] newNumber = new int [n+1];
+        newNumber[0] = carry;
+        
+        return newNumber;
+
     }
 }
