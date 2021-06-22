cfab3a4c6001fb05e6856a07867a79b9944ccc54
Sambaran Sengupta <63100608+sambarannnn@users.noreply.github.com>
2021-06-22 09:42:19 +0530

    Update Solution.java

--- Easy/122. Best Time to Buy and Sell Stock II/Solution.java
+++ Easy/122. Best Time to Buy and Sell Stock II/Solution.java
@@ -1,13 +1,31 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int maxprofit = 0;
         
+        /*
+        int maxprofit = 0;
         for(int i = 1; i < prices.length; i++) {
             //count as profit if price went up in given day
             if (prices[i] > prices[i - 1])
                 maxprofit += prices[i] - prices[i - 1];
         }
         return maxprofit;
+        */
+        int i = 0, buy, sell, profit = 0, N = prices.length - 1;
+        while (i < N) {
+            
+            //find first min price to buy
+            while (i < N && prices[i + 1] <= prices[i]) 
+                i++;
+            buy = prices[i];
+            
+            //find first max price to sell
+            while (i < N && prices[i + 1] > prices[i]) 
+                i++;
+            sell = prices[i];
+
+            profit += sell - buy;
+        }
+        return profit;
 
     }
 }
