3d3e833d556b02b9118f7a40c6ba5475e3ed1f66
Sambaran Sengupta <63100608+sambarannnn@users.noreply.github.com>
2021-06-22 06:47:23 +0530

    Update Solution.java

--- Easy/100. Same Tree/Solution.java
+++ Easy/100. Same Tree/Solution.java
@@ -14,6 +14,47 @@
  * }
  */
 class Solution {
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean ITRisSameTree(TreeNode p, TreeNode q) {
+        //ITERATIVE SOLUTION
+        //BFS
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(p);
+        queue.add(q);
+        while(!queue.isEmpty()) {
+            TreeNode t1 = queue.poll();
+            TreeNode t2 = queue.poll();
+            
+            if(t1 == null && t2 == null)
+                continue;
+            
+            if(t1 == null || t2 == null)
+                return false;
+            
+            if(t1.val != t2.val)
+                return false;
+            queue.add(t1.left);
+            queue.add(t2.left);
+            queue.add(t1.right);
+            queue.add(t2.right);
+        }
+        return true;
+    }
     public boolean isSameTree(TreeNode p, TreeNode q) {
     
         // Equal nullity denotes that this branch is the same (local equality)
