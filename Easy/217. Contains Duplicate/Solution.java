dec02dd83dd0849e4445e8993d6c90348a4765fa
Sambaran Sengupta <63100608+sambarannnn@users.noreply.github.com>
2021-06-23 13:38:05 +0530

    Update Solution.java

--- Easy/217. Contains Duplicate/Solution.java
+++ Easy/217. Contains Duplicate/Solution.java
@@ -1,6 +1,16 @@
 class Solution {
+    
     public boolean containsDuplicate(int[] nums) {
         //we can also sort and check but that would be nlogn approach as sorting is nlogn plus another O(n) for iterating through the sorted array checking for equal adjacent elements
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; ++i) {
+            if (nums[i] == nums[i + 1]) return true;
+        }
+        return false;
+    }
+
+    public boolean HASHcontainsDuplicate(int[] nums) {
+        
         
         //O(N)
         HashSet<Integer> set = new HashSet<>(nums.length);
