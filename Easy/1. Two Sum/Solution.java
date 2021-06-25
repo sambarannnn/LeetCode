2a6dbf56f5eb6a9cae8de661ff4dd5411e6d815d
Sambaran Sengupta <63100608+sambarannnn@users.noreply.github.com>
2021-06-25 08:23:39 +0530

    Update Solution.java

--- Easy/1. Two Sum/Solution.java
+++ Easy/1. Two Sum/Solution.java
@@ -1,6 +1,56 @@
 import java.util.*;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        /*
+        //generic method
+        if(nums == null)
+	    		return null;
+        
+        int[] nums2 = Arrays.copyOf(nums, nums.length);
+        //since sorting will change the index order
+        Arrays.sort(nums);
+        int p1 = 0;
+        int p2 = nums.length - 1;
+        int a = 0;
+        int b = 0;
+        while(p1 < p2) {
+            if(nums[p1] + nums[p2] == target) {
+                a = nums[p1];
+                b = nums[p2];
+                break;
+            } else if(p1 < p2 && nums[p1] + nums[p2] < target) {
+                p1++;
+            } else if(p1 < p2 && nums[p1] + nums[p2] > target) {
+                p2--;
+            }
+        }
+        int[] res = new int[2];
+        
+        for(int i = 0; i < nums2.length; i++){
+	    	if(nums2[i] == a){
+	    		res[0] = i;
+				break;
+    		}
+	    }
+        //System.out.println(a +"\n"+ b);
+        //for target 6, if we had 3,3; we would need to find index of 2d occurence
+        if(a != b){
+	    		for(int i = 0; i < nums2.length; i++){
+		    		if(nums2[i] == b){
+		    			res[1] = i;
+		    			break;
+		    		}
+		    	}
+	    } else {
+            for(int i = 0; i < nums2.length; i++){
+				if(nums2[i] == b && i != res[0]){
+	    			res[1] = i;
+	    			break;
+                }
+		    }
+        }
+        return res;
+        */
         //using hashtable or hashmap to have O(1) access to elements
         Hashtable<Integer,Integer> hm = new Hashtable<Integer,Integer>();
         int complement;
