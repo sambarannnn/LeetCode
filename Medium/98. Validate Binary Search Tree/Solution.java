d555e907cce77bc097e8b6f373c6a1d84a082c1d
Sambaran Sengupta <63100608+sambarannnn@users.noreply.github.com>
2021-06-30 15:13:37 +0530

    Update Solution.java

--- Medium/98. Validate Binary Search Tree/Solution.java
+++ Medium/98. Validate Binary Search Tree/Solution.java
@@ -15,26 +15,29 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        return recursivecheck(root, Long.MIN_VALUE, Long.MAX_VALUE);
-        // if (root == null) 
-        //     return true;
-        // Stack<TreeNode> stack = new Stack<TreeNode>();
-        // TreeNode prev = null;
-        // while(root != null || !stack.isEmpty()) {
-        //     while(root != null) {
-        //         stack.push(root);
-        //         root = root.left;
-        //     }
-        //     root = stack.pop();
-        //     //System.out.println(root.val);//inorder printing
-        //      if(prev != null && root.val <= prev.val)
-        //          return false;
-        //      prev = root;
-        //     root = root.right;
-        // }
-        // return true;
+        //return recursivecheck(root, Long.MIN_VALUE, Long.MAX_VALUE);
+        if (root == null) 
+            return true;
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode prev = null;
+        while(root != null || !stack.isEmpty()) {
+            while(root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            //before moving tonext node in inorder traversal, check if previous node is bigger than curr node
+            //System.out.println(root.val);//inorder printing
+             if(prev != null && root.val <= prev.val)
+                 return false;
+             prev = root;
+            root = root.right;
+        }
+        return true;
     }
     public boolean recursivecheck(TreeNode root, long min, long max) {
+    //for left childs, min value is long.min, max value is parent of that node
+    //for right childs, min value is parent of that node, max value is long.max
         if(root == null)
             return true;
         
@@ -44,3 +47,29 @@ class Solution {
         return recursivecheck(root.left, min, root.val) && recursivecheck(root.right, root.val, max);
     }
 }
+/*
+public class Solution {
+    //inorder traversal stored in list, then we check if traversal yields sorted result
+    List<Integer> inorder = new ArrayList<Integer>();
+    
+    public boolean isValidBST(TreeNode root) {
+        InOrder(root);
+        
+        for (int i = 0; i < inorder.size() - 1; i++) {
+            if (inorder.get(i) >= inorder.get(i+1))
+                return false;
+        }
+        
+        return true;
+    }
+    
+    void InOrder(TreeNode node) {
+        if (node == null) 
+            return;
+        
+        InOrder(node.left);
+        inorder.add(node.val);
+        InOrder(node.right);
+    }
+}
+*/
