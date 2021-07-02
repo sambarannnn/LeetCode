6f40d33aca0533f838a96f9b25880287cc0df56b
Sambaran Sengupta <63100608+sambarannnn@users.noreply.github.com>
2021-07-02 15:58:24 +0530

    Update Solution.java

--- Medium/162. Find Peak Element/Solution.java
+++ Medium/162. Find Peak Element/Solution.java
@@ -1,9 +1,26 @@
 public class Solution {
     public int findPeakElement(int[] nums) {
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] > nums[i + 1])
-                return i;
+        //plot as a graph to visualise binary search approach
+        
+        int left = 0;
+        int right = nums.length - 1;
+        
+        while(left < right) {
+            int mid = (left + right) / 2;
+            
+            if(nums[mid] < nums[mid+1]) {//there has to exist a peak to the right
+                left = mid+1;
+            } else {//there has to exist a peak to the left(or itself is a peak)
+                right = mid;
+            }
         }
-        return nums.length - 1;
+        return left;//or return right
+        
+        // //O(N^2)
+        // for (int i = 0; i < nums.length - 1; i++) {
+        //     if (nums[i] > nums[i + 1])
+        //         return i;
+        // }
+        // return nums.length - 1;
     }
 }
