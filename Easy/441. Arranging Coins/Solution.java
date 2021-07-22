6d200fa27cf60294b72aed6503b4964c0a0ab24d
Sambaran Sengupta <63100608+sambarannnn@users.noreply.github.com>
2021-07-22 22:52:32 +0530

    Update Solution.java

--- Easy/441. Arranging Coins/Solution.java
+++ Easy/441. Arranging Coins/Solution.java
@@ -1,12 +1,36 @@
 class Solution {
-    public int arrangeCoins(int n) {
-        int levelCount = 0;
-        int level = 1;
-        while(n>=0){            
-            n = n - level;//subtract max coins for this level
-            levelCount++;
-            level++;
+      public int arrangeCoins(int n) {
+          // the final answer will be 1 + 2 + 3 + .... kthlevel <= n
+          // k*(k+1)/2 <= n
+          //find tha value of such a k between 1 and n
+        long left = 1, right = n;
+        long levelk;
+        long curr;
+        
+        while (left <= right) {
+          levelk = left + (right - left) / 2;
+          curr = levelk * (levelk + 1) / 2;
+
+          if (curr == n) return (int)levelk;
+
+          if (n < curr) {
+            right = levelk - 1;
+          } else {
+            left =levelk + 1;
+          }
         }
-        return levelCount-1;
+        return (int)right;//in case we don't find exact equal value
     }
 }
+// class Solution {
+//     public int arrangeCoins(int n) {
+//         int levelCount = 0;
+//         int level = 1;
+//         while(n>=0){            
+//             n = n - level;//subtract max coins for this level
+//             levelCount++;
+//             level++;
+//         }
+//         return levelCount-1;
+//     }
+// }
