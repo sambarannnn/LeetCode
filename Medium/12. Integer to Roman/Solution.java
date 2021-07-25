1818f0e770676d72628c538408708c63c08ab8c0
Sambaran Sengupta <63100608+sambarannnn@users.noreply.github.com>
2021-07-25 18:31:22 +0530

    Create Solution.java

---
+++ Medium/12. Integer to Roman/Solution.java
@@ -0,0 +1,32 @@
+class Solution {
+    
+    public String intToRoman(int num) {
+        HashMap<String, Integer> romans = new LinkedHashMap<String, Integer>();//using linkedhashmap to maintain insertion order
+        romans.put("M", 1000);
+        romans.put("CM", 900);
+        romans.put("D", 500);
+        romans.put("CD", 400);
+        romans.put("C", 100);
+        romans.put("XC", 90);
+        romans.put("L", 50);
+        romans.put("XL", 40);
+        romans.put("X", 10);
+        romans.put("IX", 9);
+        romans.put("V", 5);
+        romans.put("IV", 4);
+        romans.put("I", 1);
+        
+        StringBuilder result = new StringBuilder();
+        
+        for(Map.Entry<String, Integer> entry : romans.entrySet()) {
+            int symbol_freq = num / entry.getValue();
+            if(symbol_freq == 0)
+                continue;
+            while(symbol_freq-- > 0) {
+                result.append(entry.getKey());
+            }
+            num = num % entry.getValue();
+        }
+        return result.toString();
+    }
+}
