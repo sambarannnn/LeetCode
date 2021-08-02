b0c301462cc2b2557fa3106c00d9baef4139182d
Sambaran Sengupta <63100608+sambarannnn@users.noreply.github.com>
2021-08-02 10:05:37 +0530

    Update Solution.java

--- Medium/78. Subsets/Solution.java
+++ Medium/78. Subsets/Solution.java
@@ -1,22 +1,31 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
-        
-        //adding first empty subset
-        result.add(new ArrayList<Integer>());
-        
-        //for each element in the set
-        for(int num : nums) {
-            int size = result.size();//tells us abt no of permutations in the result
-            
-            for(int i = 0; i < size; i++) {//for each permutation
-                List<Integer> permutation = new ArrayList<Integer>(result.get(i));//adding curr permutation(which is a list of integers)
-                //permutation.addAll(result.get(i));//adding curr permutation(which is a list of integers)
-                permutation.add(num);//making new permutation
-                
-                result.add(permutation);//adding new permutation to result list
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        res.add(new ArrayList<Integer>());
+        for(int n : nums) {
+            int size = res.size();
+            for(int i = 0; i < size; i++) {
+                List<Integer> permutation = new ArrayList<>(res.get(i));//take out each existing permutation
+                permutation.add(n);//add a new digit to it
+                res.add(permutation);//add to result, this modified permutation
             }
         }
-        return result;
+        return res;
     }
+    //BACKTRACKING
+    // public List<List<Integer>> subsets(int[] nums) {
+    //     List<List<Integer>> res = new ArrayList<List<Integer>>();
+    //     backtrack(new ArrayList<>(), res, 0, nums);
+    //     return res;
+    // }
+    // private void backtrack(List<Integer> curr, List<List<Integer>> res, int index, int[] nums){
+    //     res.add(new ArrayList<Integer>(curr));
+    //     if(index == nums.length)
+    //         return;
+    //     for(int i = index; i < nums.length; i++) {
+    //         curr.add(nums[i]);
+    //         backtrack(curr, res, i+1, nums);
+    //         curr.remove(curr.size()-1);
+    //     }
+    // }
 }
