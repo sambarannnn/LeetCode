6c6d059bb8e77c705642812fec08976139c842f8
Sambaran Sengupta <63100608+sambarannnn@users.noreply.github.com>
2021-08-13 13:32:10 +0530

    Update Solution.java

--- Easy/101. Symmetric Tree/Solution.java
+++ Easy/101. Symmetric Tree/Solution.java
@@ -15,43 +15,34 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if (root == null)
-            return true;
-        return recursiveisSymmetric(root.left, root.right);
-    }
-    public boolean recursiveisSymmetric(TreeNode p, TreeNode q) {
-        if(p == null && q == null)
-            return true;
-        if(p == null || q == null)
-            return false;
-        if(p.val != q.val)
-            return false;
-            
-        return recursiveisSymmetric(p.left, q.right) && recursiveisSymmetric(p.right, q.left);
-    }
-    public boolean ITERATIVEisSymmetric(TreeNode root) {
         if(root == null)
             return true;
-        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        Queue<TreeNode> queue = new LinkedList<>();
         queue.add(root.left);
         queue.add(root.right);
         while(!queue.isEmpty()) {
-            TreeNode t1 = queue.poll();
-            TreeNode t2 = queue.poll();
-            
-            if(t1 == null && t2 == null)
+            TreeNode root1 = queue.remove();
+            TreeNode root2 = queue.remove();
+            if(root1 == null && root2 == null)
                 continue;
-            
-            if(t1 == null || t2 == null)
+            if(root1 == null || root2 == null)
                 return false;
-            
-            if(t1.val != t2.val)
+            if(root1.val != root2.val)
                 return false;
-            queue.add(t1.left);
-            queue.add(t2.right);
-            queue.add(t1.right);
-            queue.add(t2.left);
+            queue.add(root1.left);
+            queue.add(root2.right);
+            queue.add(root1.right);
+            queue.add(root2.left);
         }
         return true;
+        // return RecursiveCheck(root.left, root.right);
+    }
+    private boolean RecursiveCheck(TreeNode root1, TreeNode root2) {
+        if(root1 == null && root2 == null)
+            return true;
+        if(root1 == null || root2 == null)
+            return false;
+        
+        return root1.val == root2.val && RecursiveCheck(root1.left, root2.right) && RecursiveCheck(root1.right, root2.left);
     }
 }
