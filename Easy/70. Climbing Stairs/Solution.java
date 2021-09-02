ab83d02669dc9719b710e8bd2fcb48d6126a4b36
Sambaran Sengupta <63100608+sambarannnn@users.noreply.github.com>
2021-09-02 21:19:30 +0530

    Update Solution.java

--- Easy/70. Climbing Stairs/Solution.java
+++ Easy/70. Climbing Stairs/Solution.java
@@ -1,23 +1,25 @@
 class Solution {
+    private int[] memo;
     public int climbStairs(int n) {
-        //for any stair at nth level, 
-        //no of ways to reach that is either from previous level(1 stair) or from prev to previous level(2 stairs).
-        //for 1 stair, only 1 way to climb that is w 1 step
-        //for 2 stairs, 2 ways to climb i.e., 1+1(2steps) or 2(single step)
-        //for nth stair no of possible ways = possible ways for (n-1) + possible ways for (n-2)
-        if(n <= 0) return 0;
-        if(n == 1) return 1;
-        if(n == 2) return 2;
-        //imagine you are at step no 3(n = 3)
-        int one_step_before = 2;//ways to reach step no 2
-        int two_steps_before = 1;//ways to reach step no 1
-        int all_ways = 0;
-        
-        for(int i=2; i<n; i++){
-            all_ways = one_step_before + two_steps_before;
-            two_steps_before = one_step_before;
-            one_step_before = all_ways;
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i < dp.length; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
         }
-        return all_ways;
+        return dp[n];
+    }
+    public int climbStairsMEMO(int n) {
+        memo = new int[n+1];
+        return steps(n);
+    }
+    private int steps(int n) {
+        
+        if(n == 0 || n == 1) {
+            return 1;
+        }
+        if(memo[n] != 0)
+            return memo[n];
+        return memo[n] = steps(n-1) + steps(n-2);
     }
 }
