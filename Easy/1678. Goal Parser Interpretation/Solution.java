570b4cc9125ec840f3d260b15d9c3a543397b2e3
Sambaran Sengupta <63100608+sambarannnn@users.noreply.github.com>
2022-03-14 10:48:20 +0530

    Create Solution.java

---
+++ Easy/1678. Goal Parser Interpretation/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String interpret(String command) {
+        StringBuilder res = new StringBuilder();
+        int i = 0;
+        while(i < command.length()) {
+            if(command.charAt(i) == 'G') {
+                res.append('G');
+                i += 1;
+            } else if(command.charAt(i) == '(') {
+                if(command.charAt(i+1) == ')') {
+                    res.append('o');
+                    i += 2;
+                } else {
+                    res.append("al");
+                    i += 4;
+                }
+            }
+        }
+        return res.toString();
+    }
+}
