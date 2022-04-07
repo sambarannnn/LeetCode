701d665ecf4d25f3675758e62e654332d97719b4
Sambaran Sengupta <63100608+sambarannnn@users.noreply.github.com>
2022-04-07 10:32:48 +0530

    Update Solution.java

--- Easy/1046. Last Stone Weight/Solution.java
+++ Easy/1046. Last Stone Weight/Solution.java
@@ -1,15 +1,15 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
-        PriorityQueue<Integer> maxheap = new PriorityQueue<Integer>((a,b) -> b-a);
-        
+        PriorityQueue<Integer> maxheap = new PriorityQueue<Integer>((a, b) -> b-a);
         for(int stone : stones) {
             maxheap.add(stone);
         }
+        
         while(maxheap.size() > 1) {
-            int y = maxheap.remove();
+            int y = maxheap.remove();//bigger no at root
             int x = maxheap.remove();
-            maxheap.add(y-x);
+            maxheap.add(y-x);//if y and x are equal it will add 0
         }
-        return maxheap.remove();
+        return maxheap.peek();
     }
 }
