5c7658659f149915320765e46d1d739878ceb85e
Sambaran Sengupta <63100608+sambarannnn@users.noreply.github.com>
2022-04-13 16:09:17 +0530

    Update Solution.java

--- Medium/59. Spiral Matrix II/Solution.java
+++ Medium/59. Spiral Matrix II/Solution.java
@@ -35,3 +35,49 @@ class Solution {
         return matrix;
     }
 }
+/*
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] matrix = new int[n][n];
+        
+        int r = 0;
+        int c = 0;
+        int dir = 1;//1 = right, left = -1, up = 2, down = -2
+        int curr = 1;
+        int max = n*n;
+        fill(matrix, n, r, c, dir, curr, max);
+        return matrix;
+    }
+    public static void fill(int[][] matrix, int n, int r, int c, int dir, int curr, int max) {
+        if(curr > max) {
+            return;
+        }
+        matrix[r][c] = curr;
+        if(dir == 1) {
+            if(c == n-1 || matrix[r][c+1] != 0) {
+                fill(matrix, n, r+1, c, -2, curr+1, max);
+            } else {
+                fill(matrix, n, r, c+1, 1, curr+1, max);
+            }
+        } else if(dir == -2) {
+            if(r == n-1 || matrix[r+1][c] != 0) {
+                fill(matrix, n, r, c-1, -1, curr+1, max);
+            } else {
+                fill(matrix, n, r+1, c, -2, curr+1, max);
+            }
+        } else if(dir == -1) {
+            if(c == 0 || matrix[r][c-1] != 0) {
+                fill(matrix, n, r-1, c, 2, curr+1, max);
+            } else {
+                fill(matrix, n, r, c-1, -1, curr+1, max);
+            }
+        } else if(dir == 2) {
+            if(r == 0 || matrix[r-1][c] != 0) {
+                fill(matrix, n, r, c+1, 1, curr+1, max);
+            } else {
+                fill(matrix, n, r-1, c, 2, curr+1, max);
+            }
+        }
+    }
+}
+*/
