11d55995a17a78c06e3c2fa8d8baf2b7f47b7929
Sambaran Sengupta <63100608+sambarannnn@users.noreply.github.com>
2022-05-14 21:57:15 +0530

    Create Solution.java

---
+++ Easy/2270. Number of Ways to Split Array/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int waysToSplitArray(int[] nums) {
+        if(nums.length==1) {
+            return 0;
+        }
+        long sum = 0;
+        for(int num : nums) {
+            sum += num;
+        }
+        
+        long leftSum = 0;
+        int res = 0;
+        for(int i = 0; i < nums.length-1; i++) {
+            leftSum += nums[i];
+            long rightSum = sum - leftSum;
+            if(leftSum >= rightSum) {
+                res++;
+            }
+        }
+        return res;
+    }
+}
